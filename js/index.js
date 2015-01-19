8c4cab224dc0814fa4939f37129e7ecf8261ccc8
yoyo930021 <yoyo930021@gmail.com>
2015-01-20 00:26:32 +0800

    完成主要進入網頁 index.htm 第一版

---
+++ js/index.js
@@ -0,0 +1,27 @@
+var s = 1;
+var f = 0;
+$(document).ready(function () {
+	$("#groupbutton").hide();
+	$("#grtop").hide();
+	$("#title").hide();
+	var $ofweidht = innerWidth;
+	if ($ofweidht <= 767) {
+		$("#mainback").hide();
+	} else {
+		$('#mainback').cycle({
+			fx: 'fade',
+			timeout: 5000,
+			random: 1
+		});
+	}
+});
+
+function start1() {
+	var t = setTimeout("start()", 500);
+}
+
+function start() {
+	$("#title").fadeIn(1200);
+	$("#groupbutton").fadeIn(1000);
+	$("#grtop").fadeIn(1000);
+}
