2a67b71569d5ab938b6bd19f9a468b4b33cddfb6
LaLa <happy369987@gmail.com>
2015-02-07 19:10:12 +0800

    oaoaoaoa

---
+++ js/work.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $("#emenu1").hide();
+    $("#emenu2").hide();
+    $("#emenu3").hide();
+    $("#emenu4").hide();
+    $("#emenu5").hide();
+
+    var sec=500;
+
+    $("mbutton1").click(function(){
+        $("#emenu1").fadeIn(sec);
+        $("#emenu2").hide();
+        $("#emenu3").hide();
+        $("#emenu4").hide();
+        $("#emenu5").hide();
+    });
+    $("mbutton2").click(function(){
+        $("#emenu2").fadeIn(sec);
+        $("#emenu1").hide();
+        $("#emenu3").hide();
+        $("#emenu4").hide();
+        $("#emenu5").hide();
+    });
+    $("mbutton3").click(function(){
+        $("#emenu3").fadeIn(sec);
+        $("#emenu1").hide();
+        $("#emenu2").hide();
+        $("#emenu4").hide();
+        $("#emenu5").hide();
+    });
+    $("mbutton4").click(function(){
+        $("#emenu4").fadeIn(sec);
+        $("#emenu1").hide();
+        $("#emenu2").hide();
+        $("#emenu3").hide();
+        $("#emenu5").hide();
+    });
+    $("mbutton5").click(function(){
+        $("#emenu5").fadeIn(sec);
+        $("#emenu1").hide();
+        $("#emenu2").hide();
+        $("#emenu3").hide();
+        $("#emenu4").hide();
+    });
+});
