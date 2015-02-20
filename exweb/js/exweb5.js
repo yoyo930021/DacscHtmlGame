de1ea2400ee996bb2c367f7c41c413fa7aa96c02
LaLa <happy369987@gmail.com>
2015-02-20 12:46:22 +0800

    2.2

---
+++ exweb/js/exweb5.js
@@ -0,0 +1,40 @@
+var con1_1=0;
+var con1_2=0;
+var mt=500;
+var ext= 1000;
+
+$("#mbody").ready(function(){
+    con1_1=0;
+    con1_2=0;
+    $("#extramenu").hide();
+    $("#content1_1").hide();
+    $("#content1_2").hide();
+    $("#extramenu").slideDown(mt);
+
+    $("#midmenu1_1").click(function(){
+        if(con1_1==0)
+        {
+            $("#content1_1").slideDown(ext);
+            con1_1=1;
+        }
+        else
+        {
+            $("#content1_1").slideUp(ext);
+            con1_1=0;
+        }
+    });
+
+    $("#midmenu1_2").click(function(){
+        if(con1_2==0)
+        {
+            $("#content1_2").slideDown(ext);
+            con1_2=1;
+        }
+        else
+        {
+            $("#content1_2").slideUp(ext);
+            con1_2=0;
+        }
+    });
+
+});
