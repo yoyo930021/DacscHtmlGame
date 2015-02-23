94f5c3073252048a3c36984d9f77102760d6b248
happy <happy369987@gmail.com>
2015-02-23 14:12:10 +0800

    2.5

--- js/narrative.js
+++ js/narrative.js
@@ -2,12 +2,22 @@ var s = 0;
 var t = 0;
 var y = 0;
 var b = 0;
+var p1,p2,p3,p4;
+var y1,y2,y3,y4;
 $(document).ready(function () {
+    p1=$("#text1").offset();
+    p2=$("#text2").offset();
+    p3=$("#text3").offset();
+    p4=$("#text4").offset();
+    /*y1=p1.top();
+    y2=p2.top();
+    y3=p3.top();
+    y4=p4.top();*/
     $(window).scroll(function () {
             $("#mainback").css("top",$(window).scrollTop());
         if (b == 0) {
             var A = $(this).scrollTop();
-            if (A < document.body.clientHeight * 1) {
+            if (A < 55) {
                 $("#hdbtn1").css("text-shadow", "0px 0px 8px #f00");
                 $("#hdbtn2").css("text-shadow", "");
                 $("#hdbtn3").css("text-shadow", "");
